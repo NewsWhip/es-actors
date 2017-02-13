8296c69186786ad2c54506267c7c4dd9fee8924d
Ross Andreucetti <ross.andreucetti@newswhip.com>
2017-02-13 17:07:44 +0000

    prep for upgrade;

--- es-actors/project/Settings.scala
+++ es-actors/project/Settings.scala
@@ -28,7 +28,7 @@ object Settings extends Dependencies {
 
   private val commonSettings = Seq(
     organization := defaultOrg,
-    version := "1.3.24-kibana",
+    version := "1.3.25-kibana",
     scalaVersion := scalaVersionUsed
   )
 
