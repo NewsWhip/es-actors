01410789bd22c4786e8311f08c7c000d6426ae16
Ross Andreucetti <ross.andreucetti@newswhip.com>
2017-03-01 10:02:50 +0000

    cahnge back to scan

--- es-actors/project/Settings.scala
+++ es-actors/project/Settings.scala
@@ -28,7 +28,7 @@ object Settings extends Dependencies {
 
   private val commonSettings = Seq(
     organization := defaultOrg,
-    version := "1.3.25-kibana",
+    version := "1.3.26-kibana",
     scalaVersion := scalaVersionUsed
   )
 
