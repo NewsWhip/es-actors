cf762d1ddec99f25161f2c3d9473b4e5576d6004
Ross Andreucetti <ross.andreucetti@newswhip.com>
2017-03-06 16:08:21 +0000

    change bulk size

--- es-actors/project/Settings.scala
+++ es-actors/project/Settings.scala
@@ -28,7 +28,7 @@ object Settings extends Dependencies {
 
   private val commonSettings = Seq(
     organization := defaultOrg,
-    version := "1.3.27-kibana",
+    version := "1.3.28-kibana",
     scalaVersion := scalaVersionUsed
   )
 
