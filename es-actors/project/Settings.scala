ff34b259f9d78d60f1957b418ef945f731bd186f
Ross Andreucetti <ross.andreucetti@newswhip.com>
2017-03-06 16:11:25 +0000

    fix compile err

--- es-actors/project/Settings.scala
+++ es-actors/project/Settings.scala
@@ -28,7 +28,7 @@ object Settings extends Dependencies {
 
   private val commonSettings = Seq(
     organization := defaultOrg,
-    version := "1.3.28-kibana",
+    version := "1.3.29-kibana",
     scalaVersion := scalaVersionUsed
   )
 
