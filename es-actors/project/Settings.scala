5b6803e358a39f152993c1113e9a90946aba0802
Ross Andreucetti <ross.andreucetti@newswhip.com>
2017-03-20 09:36:17 +0000

    add script to terminate instance at 12:45

--- es-actors/project/Settings.scala
+++ es-actors/project/Settings.scala
@@ -28,7 +28,7 @@ object Settings extends Dependencies {
 
   private val commonSettings = Seq(
     organization := defaultOrg,
-    version := "1.3.32-kibana",
+    version := "1.3.33-kibana",
     scalaVersion := scalaVersionUsed
   )
 
