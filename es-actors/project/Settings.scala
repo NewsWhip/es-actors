218e431204f2698211694625851ad8402e5e32d1
Ross Andreucetti <ross.andreucetti@newswhip.com>
2017-04-06 16:26:02 +0100

    add better failover handling

--- es-actors/project/Settings.scala
+++ es-actors/project/Settings.scala
@@ -28,7 +28,7 @@ object Settings extends Dependencies {
 
   private val commonSettings = Seq(
     organization := defaultOrg,
-    version := "1.3.33",
+    version := "1.3.34",
     scalaVersion := scalaVersionUsed
   )
 
