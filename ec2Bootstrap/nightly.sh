f2813f36cd9d2225c7411fa216077a95200e4d1c
Gabriel Broilo <gabriel.broilo@newswhip.com>
2017-04-10 10:47:29 +0100

    Update userData_alternative.sh

--- ec2Bootstrap/nightly.sh
+++ ec2Bootstrap/nightly.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# 45 0 * * * /opt/elasticsearch-migration/es-actors-1.3.7/ec2Bootstrap/nightly.sh /opt/elasticsearch-migration/es-actors-1.3.7/ NewsWhipCluster NewsWhipStagingCluster 10.0.1.10,10.0.3.10,10.0.7.10,10.0.9.10 10.0.1.110,10.0.3.110,10.0.7.110,10.0.9.110 9300 8 >/dev/null 2>&1
+# 45 0 * * * /opt/elasticsearch-migration/es-actors-VERSION/ec2Bootstrap/nightly.sh /opt/elasticsearch-migration/es-actors-1.3.7/ NewsWhipCluster NewsWhipStagingCluster 10.0.1.10,10.0.3.10,10.0.7.10,10.0.9.10 10.0.1.110,10.0.3.110,10.0.7.110,10.0.9.110 9300 4 >/dev/null 2>&1
 
 # Setup cronjob with this script to run
 DATE_START=$(date +%Y-%m-%d)
@@ -17,7 +17,7 @@ NIGHTLY="--nightly date=$DATE_START,weeksBack=$WEEKS"
 
 echo "Executing nightly script with date: $DATE_START and weeks: $WEEKS"
 
-CMD="sbt -J-Xmx25G -J-Xms25G \"; project client; run-main com.broilogabriel.Client --sourceCluster=$SOURCE_CLUSTER --targetCluster=$TARGET_CLUSTER --sources=$SOURCE_IPS --targets=$TARGET_IPS $NIGHTLY $INDEX\""
+CMD="sbt -J-Xmx25G -J-Xms25G \"project client\" run --sourceCluster=$SOURCE_CLUSTER --targetCluster=$TARGET_CLUSTER --sources=$SOURCE_IPS --targets=$TARGET_IPS $NIGHTLY $INDEX\""
 
 echo "$CMD"
 cd $CLIENT_PROJECT && eval $CMD
--- ec2Bootstrap/userData_alternative.sh
+++ ec2Bootstrap/userData_alternative.sh
@@ -9,10 +9,10 @@ apt_get_install()
         install $@
 }
 
-ES_ACTORS_VERSION=1.3.32
+ES_ACTORS_VERSION=v2.0-alpha
 
 # Mark execution start
-echo "STARTING" > /root/user_data_run
+echo "STARTING" >> /root/user_data_run
 
 # Some initial setup
 set -e -x
@@ -37,27 +37,19 @@ apt_get_install sbt
 
 mkdir /opt/elasticsearch-migration
 cd /opt/elasticsearch-migration
-# first setup Spike -> Analytics
+
+# Setup server
 wget https://github.com/NewsWhip/es-actors/archive/v$ES_ACTORS_VERSION.tar.gz
 tar -xvf v$ES_ACTORS_VERSION.tar.gz
 cd es-actors-$ES_ACTORS_VERSION/es-actors
-SERVER_CMD="sbt -J-Xmx25G -J-Xms25G \"; project server; run-main com.broilogabriel.Server\""
+SERVER_CMD="sbt -J-Xmx25G -J-Xms25G \"project server\" run"
 eval $SERVER_CMD </dev/null &>/dev/null &
 chmod 755 /opt/elasticsearch-migration/es-actors-$ES_ACTORS_VERSION/ec2Bootstrap/nightly.sh
-# next setup Spike -> Kibana
-cd /opt/elasticsearch-migration
-wget https://github.com/NewsWhip/es-actors/archive/v$ES_ACTORS_VERSION-kibana.tar.gz
-tar -xvf v$ES_ACTORS_VERSION-kibana.tar.gz
-cd es-actors-$ES_ACTORS_VERSION-kibana/es-actors
-SERVER_CMD="sbt -J-Xmx25G -J-Xms25G \"; project server; run-main com.broilogabriel.Server\""
-eval $SERVER_CMD </dev/null &>/dev/null &
-chmod 755 /opt/elasticsearch-migration/es-actors-$ES_ACTORS_VERSION-kibana/ec2Bootstrap/nightly.sh
 
-# Finally let's setup our cronjobs
+# Finally let's setup the cronjob
 sleep 100
-echo "45 0 * * * root /opt/elasticsearch-migration/es-actors-$ES_ACTORS_VERSION/ec2Bootstrap/nightly.sh /opt/elasticsearch-migration/es-actors-$ES_ACTORS_VERSION/es-actors NewsWhipCluster NewsWhipStagingCluster 10.0.1.10,10.0.3.10,10.0.7.10,10.0.9.10 10.0.1.110,10.0.3.110,10.0.7.110,10.0.9.110,10.0.1.111 9300 8 >/dev/null 2>&1 &" >> /etc/crontab
-echo "45 0 * * * root /opt/elasticsearch-migration/es-actors-$ES_ACTORS_VERSION-kibana/ec2Bootstrap/nightly.sh /opt/elasticsearch-migration/es-actors-$ES_ACTORS_VERSION-kibana/es-actors NewsWhipCluster NewsWhipKibanaCluster 10.0.1.10,10.0.3.10,10.0.7.10,10.0.9.10 10.0.9.14,10.0.1.15,10.0.7.15 9300 8 >/dev/null 2>&1 &" >> /etc/crontab
+echo "45 0 * * * root /opt/elasticsearch-migration/es-actors-$ES_ACTORS_VERSION/ec2Bootstrap/nightly.sh /opt/elasticsearch-migration/es-actors-$ES_ACTORS_VERSION/es-actors NewsWhipCluster NewsWhipStagingCluster 10.0.1.10,10.0.3.10,10.0.7.10,10.0.9.10 10.0.1.110,10.0.3.110,10.0.7.110,10.0.9.110,10.0.1.111 9300 4 >/dev/null 2>&1 &" >> /etc/crontab
 
 # Mark execution end
-echo "DONE" > /root/user_data_run
+echo "DONE" >> /root/user_data_run
 
